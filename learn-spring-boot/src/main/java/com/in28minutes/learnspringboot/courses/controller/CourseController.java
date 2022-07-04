c8c788ff8420f04e6ba139bedf5e0fd8e71fc845
Deekshithrathod <68671324+Deekshithrathod@users.noreply.github.com>
2022-07-04 12:19:44 +0530

    Created my first REST API with spring boot

---
+++ learn-spring-boot/src/main/java/com/in28minutes/learnspringboot/courses/controller/CourseController.java
@@ -0,0 +1,22 @@
+package com.in28minutes.learnspringboot.courses.controller;
+
+import com.in28minutes.learnspringboot.courses.bean.Course;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    @GetMapping("/courses")
+    public List<Course> getAllCourses(){
+        return Arrays.asList(new Course(1,"Course1", "Author1"),new Course(2,"Course2", "Author2"));
+    }
+
+    @GetMapping("/courses/1")
+    public Course getOneCourse(){
+        return new Course(1,"Course1", "Author1");
+    }
+}
